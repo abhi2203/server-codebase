0a22bdc2133ed75281d683ec3957474c85c1d4e6
abhi2203 <30718794+abhi2203@users.noreply.github.com>
2022-05-13 15:06:45 +0530

    Update EmployeeController.java

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -19,7 +19,7 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@PostMapping("/submit")
+	@PostMapping("/add")
 	public String saveNewRegistration(@RequestBody Employee employee){
 		
 		String message =this.employeeService.saveEmployee(employee);
